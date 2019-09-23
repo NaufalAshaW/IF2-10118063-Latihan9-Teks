c8fb9b1cc29620af6506e4a972928c32235659f0
NaufalAshaW <55681676+NaufalAshaW@users.noreply.github.com>
2019-09-23 19:57:06 +0700

    Add files via upload

---
+++ src/if2/pkg10118063/latihan9/IF210118063Latihan9.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2.pkg10118063.latihan9;
+
+/**
+ *
+ * @author 
+ * NAMA   : Naufal Asha
+ * KELAS  : IF-2
+ * NIM    : 10118063
+ */
+public class IF210118063Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        char c;
+        char tab = '\t';
+        char newline = '\n';
+        c = 'A';
+        System.out.println("Hasil" + newline + "Char:" + tab + "berisi " + c);
+    }
+    
+}
